8014d6bb754177388afc9a648b58b590e1f5c6ae
Suman Khara <suman.khara@cerner.com>
2020-12-23 20:45:45 +0530

    format

--- src/main/java/com/optimagrowth/authentication/security/OAuth2Config.java
+++ src/main/java/com/optimagrowth/authentication/security/OAuth2Config.java
@@ -11,23 +11,22 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+  @Autowired private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+  @Autowired private UserDetailsService userDetailsService;
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("ostock")
-                .secret("{noop}thisissecret")
-                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
-                .scopes("webclient", "mobileclient");
-    }
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+        .inMemory()
+        .withClient("ostock")
+        .secret("{noop}thisissecret")
+        .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+        .scopes("webclient", "mobileclient");
+  }
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
-    }
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+  }
 }
